87c8b6bcf4a07b06c3a8bfe724130dc979e6355f
Erik Gaius Capistrano <gaiussensei@gmail.com>
2014-08-16 17:00:26 +0800

    Install bower components

--- Project Manila/js/index.js
+++ Project Manila/js/index.js
@@ -1,1 +1,8 @@
-alert("Hello, World!");
+head.load("bower_components/jquery/dist/jquery.min.js", //
+    "bower_components/bootstrap/dist/css/bootstrap.min.css", //
+    "bower_components/bootstrap/dist/js/bootstrap.min.js",
+
+    function readyF() {
+        alert("Hello, World!");
+    }
+);
